e0762eac99ec38a5c80e85df92506c47cc809bd7
Alexandria P <alxpettit@gmail.com>
2023-10-24 02:12:27 -0700

    no unsafe ever

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,44 @@ use std::{rc::Rc, sync::RwLock};
 
 #[derive(Default)]
 pub enum BounceState {
-    #[default]
     Reverse,
+    #[default]
     Forward,
 }
 
 // TODO: when I have some caffeine, come up with a better name
-pub fn rwlockify<T: Clone>(v: impl Iterator<Item = T>) -> Vec<Rc<RwLock<T>>> {
-    v.map(|x| Rc::new(RwLock::new(x.clone()))).collect()
+pub fn rwlockify<T: Clone>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = Rc<RwLock<T>>> {
+    iter.map(|x| Rc::new(RwLock::new(x.to_owned())))
+}
+
+pub fn unrwlockify<T: Clone>(iter: impl Iterator<Item = Rc<RwLock<T>>>) -> impl Iterator<Item = T> {
+    iter.map(|x| x.read().expect("Failed to read RWlock").to_owned())
 }
 
 pub struct BounceIterMut<T> {
-    slice: Vec<Rc<RwLock<T>>>,
+    collection: Vec<Rc<RwLock<T>>>,
     index: usize,
     bounce_state: BounceState,
 }
 
 impl<T> BounceIterMut<T> {
-    pub fn new(slice: Vec<Rc<RwLock<T>>>) -> Self {
+    pub fn reset(&mut self) {
+        self.index = 0;
+    }
+    pub fn reset_rev(&mut self) {
+        self.index = self.collection.len() - 1;
+    }
+    pub fn new(collection: Vec<Rc<RwLock<T>>>) -> Self {
         Self {
-            slice,
+            collection,
             index: 0,
             bounce_state: Default::default(),
         }
     }
-    pub fn new_rev(slice: Vec<Rc<RwLock<T>>>) -> Self {
-        let len = slice.len() - 1;
+    pub fn new_rev(collection: Vec<Rc<RwLock<T>>>) -> Self {
+        let len = collection.len() - 1;
         Self {
-            slice,
+            collection,
             index: len,
             bounce_state: Default::default(),
         }
@@ -40,7 +50,7 @@ impl<T> Iterator for BounceIterMut<T> {
     type Item = Rc<RwLock<T>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let len = (*self.slice).len();
+        let len = self.collection.len();
 
         if self.index >= len {
             self.bounce_state = BounceState::Reverse;
@@ -48,11 +58,7 @@ impl<T> Iterator for BounceIterMut<T> {
         } else if self.index == 0 {
             self.bounce_state = BounceState::Forward;
         }
-        // SAFETY: PhantomData locks our lifetime to the lifetime of the array pointer,
-        // so use-after-free is impossible
-        // TODO: check if multiple mutable ref possible,
-        // may undermine safety guarantees in niche ways.
-        let ret = Some(self.slice[self.index as usize].clone());
+        let ret = self.collection[self.index].clone();
 
         match self.bounce_state {
             BounceState::Reverse => {
@@ -62,50 +68,50 @@ impl<T> Iterator for BounceIterMut<T> {
                 self.index += 1;
             }
         }
-        ret
+        Some(ret)
     }
 }
 
-#[cfg(test)]
+// #[cfg(test)]
 mod tests {
-    use std::sync::{Arc, Mutex};
 
     use super::*;
     #[test]
     fn basic_test() {
         let mut data = vec![1, 2, 3, 4, 5];
         let expected = vec![1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5];
-        let mut iter = BounceIterMut::new(rwlockify(data.iter()));
-        // assert_eq!(*iter.take(13).collect::<Vec<usize>>(), expected);
+        let mut iter = BounceIterMut::new(rwlockify(data.iter()).collect());
+        assert_eq!(
+            *unrwlockify(iter).take(13).map(|x| *x).collect::<Vec<_>>(),
+            expected
+        );
     }
     #[test]
     fn basic_test_rev() {
         let mut data = vec![1, 2, 3, 4, 5];
         let expected = vec![5, 4, 3, 2, 1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5];
-        let mut iter = BounceIterMut::new_rev(rwlockify(data.iter()));
-        // assert_eq!(*iter.take(17).map(|x| *x).collect::<Vec<usize>>(), expected);
+        let mut iter = BounceIterMut::new_rev(rwlockify(data.iter()).collect());
+        assert_eq!(
+            *unrwlockify(iter).take(17).map(|x| *x).collect::<Vec<_>>(),
+            expected
+        );
     }
     #[test]
     fn write() {
-        let mut data = vec![1, 2, 3, 4, 5];
+        let ptrs: Vec<_> = rwlockify(vec![1, 2, 3, 4, 5].into_iter()).collect();
         let expected = vec![2, 4, 6, 8, 10];
-        let mut iter = BounceIterMut::new(rwlockify(data.iter()));
-        for item in iter.take(5) {
-            let item = item.lock();
-            let value = *item;
-            *item = value * 2;
+        let mut iter = BounceIterMut::new(ptrs);
+        for _ in 0..5 {
+            let Some(item) = iter.next() else {
+                break;
+            };
+            dbg!(&item);
+            let value = *item.read().unwrap();
+            *item.write().unwrap() = value * 2;
+            dbg!(&item);
         }
+        iter.reset();
+        let data = unrwlockify(iter).take(5).collect::<Vec<_>>();
         assert_eq!(data, expected);
     }
-    // CORRECT: Fails due to *mut [i32] not being Send
-    // #[test]
-    // fn move_to_new_thread() {
-    //     let mut data = vec![1, 2, 3, 4, 5];
-    //     let mut iter = Arc::new(Mutex::new(BounceIterMut::new(&mut data)));
-    //     let iter_ptr = iter.clone();
-
-    //     std::thread::spawn(|| {
-    //         iter_ptr.lock().unwrap();
-    //     });
-    // }
 }
